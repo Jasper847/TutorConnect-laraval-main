dd2a7f36816804adfb720ba6048a4ff73404065d
Jasper847 <jasperai001@gmail.com>
2026-08-17 14:46:56 +0500

    fix: add fallback APP_KEY and database seed to Railway startup

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:c29tZXJhbmRvbXNlY3JldGtleXR1dG9yY29ubmVjdDExMQ=='),
 
     'previous_keys' => [
         ...array_filter(
